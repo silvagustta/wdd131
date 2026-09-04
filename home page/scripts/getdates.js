657b0e069098b7812f8c33e9cb525cbb7c4eab71
silvagustta <gustta121304@gmail.com>
2026-09-04 19:49:19 -0400

    updading JS

--- home page/scripts/getdates.js
+++ home page/scripts/getdates.js
@@ -1,6 +1,6 @@
 const currentYear = new Date().getFullYear();
 
-document.getElementById('currentYear').textContent = currentYear;
+document.getElementById('currentyear').textContent = currentYear;
 
 document,getElementById('lastModified').textContent = 
     document.lastModified;
