0b2a23a7e0d07fbd25c272f7ffe3b06b9931408e
silvagustta <gustta121304@gmail.com>
2026-09-19 17:44:02 -0400

    Enhance weather display with SVG icon and improve wind chill calculation logic

--- scripts/place.js
+++ scripts/place.js
@@ -4,33 +4,35 @@ const lastModified = new Date(document.lastModified).toLocaleString('en-US')
 
 document.getElementById('currentyear').textContent = currentYear;
 
-document.getElementById('lastModified').textContent = 
+document.getElementById('lastModified').textContent =
     lastModified;
 
 // weather
 const temperature = document.getElementById("temperature");
 const windSpeed = document.getElementById("wind-speed");
 const windChillElement = document.getElementById("wind-chill");
-const temp = 5;
-const speed = 10;
-const windChill = calculateWindChill(temp, speed);
+const temp = 10;
+const speed = 6;
+let windChill;
+
+if (temp <= 10 && speed > 4.8) {
+    windChill = calculateWindChill(temp, speed);
+} else {
+    windChill = "N/A";
+}
 
 temperature.textContent = temp;
 windSpeed.textContent = speed;
-windChillElement.textContent = windChill
+windChillElement.textContent = windChill;
 
-function calculateWindChill(temp, speed){
-    if(temp <= 10 && speed > 4.8){
-        const windFactor = Math.pow(speed, 0.16);
-        const tempFactor = 0.6215 * temp;
-        const windEffect = 11.37 * windFactor;
-        const tempWindEffect = 0.3965 * temp * windFactor;
 
-        const windChill = 13.12 +tempFactor - windEffect + tempWindEffect;
+function calculateWindChill(temp, speed) {
+    const windFactor = Math.pow(speed, 0.16);
+    const tempFactor = 0.6215 * temp;
+    const windEffect = 11.37 * windFactor;
+    const tempWindEffect = 0.3965 * temp * windFactor;
 
-        return windChill.toFixed(1);
-    }
-    else {
-        return "N/A"
-    }
+    const windChill = 13.12 + tempFactor - windEffect + tempWindEffect;
+
+    return windChill.toFixed(1);
 }
